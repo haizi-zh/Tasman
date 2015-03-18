c6757879f738e1c57f98092d4afb262337c96f17
ck <chenkaiC4@gmail.com>
2015-03-18 16:36:05 +0800

    add notification

---
+++ client/templates/notifications/notifications.js
@@ -0,0 +1,34 @@
+Template.notifications.helpers({
+  notifications: function() {
+    return Notifications.find({
+      userId: Meteor.userId(),
+      read: false
+    });
+  },
+  notificationCount: function() {
+    return Notifications.find({
+      userId: Meteor.userId(),
+      read: false
+    }).count();
+  }
+});
+
+
+Template.notificationItem.helpers({
+  notificationPostPath: function() {
+    return Router.routes.postPage.path({
+      _id: this.postId
+    });
+  }
+});
+Template.notificationItem.events({
+  'click a': function() {
+    Notifications.update({
+      _id: this._id
+    }, {
+      '$set': {
+        read: true
+      }
+    });
+  }
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -4,11 +4,15 @@ if (Posts.find().count() === 0) {
 
   // create two users
   var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
+    profile: {
+      name: 'Tom Coleman'
+    }
   });
   var tom = Meteor.users.findOne(tomId);
   var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
+    profile: {
+      name: 'Sacha Greif'
+    }
   });
   var sacha = Meteor.users.findOne(sachaId);
 
@@ -54,4 +58,15 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+
+  for (var i = 0; i < 20; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000),
+      commentsCount: 0
+    });
+  }
 }
