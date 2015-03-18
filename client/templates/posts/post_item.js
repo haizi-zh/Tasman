65e52b39b4136d9e243359f621df35e05c1ca0c7
ck <chenkaiC4@gmail.com>
2015-03-18 21:56:51 +0800

    add page controller and vote functionality

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,13 +1,32 @@
 Template.postItem.helpers({
-
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url; // this 指向当前代码快，由each分配
     return a.hostname;
   },
-
   ownPost: function() {
     return this.userId === Meteor.userId();
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvoteable';
+    } else {
+      return 'disabled';
+    }
+  },
+  hasS: function(n, text) {
+    if (n <= 1) {
+      return text;
+    } else {
+      return text + 's';
+    }
   }
+});
 
+Template.postItem.events({
+  'click .upvoteable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
 });
