2df7dc068fc2afeab60235265b4651f605b428e5
lyn <buaaxigua@gmail.com>
2015-03-28 17:56:30 +0800

    fetch remote pic and upload local pic

--- client/templates/data_review/city/pictures.js
+++ client/templates/data_review/city/pictures.js
@@ -4,30 +4,34 @@ var cropCoords = {};//记录单个crop数据
 var index = -1;//记录第几个图片
 var selected = [];
 
+
+var AccessToken = "";
+
 Template.pictures.events({
   "dblclick .raw-picture-container": function(e){
   	clearTimeout(_time);
     var $picShadow = $(e.target).parent().children(".pic-shadow");
     if ($picShadow.css("display") == "none"){
+      //insert the cropped image
       $picShadow.show();
       var imageElement = 
-        '<li class="selected-picture-container">' + 
-          '<img src="' + this.url + '?imageView2/1/w/100/h/100" width="100px" height="100px" class="img-rounded" data-id="' + this.id 
-            + '" data-index="' + this.index + '">' +
+        '<li class="selected-picture-container" data-index="' + this.index + '">' + 
+          '<img src="' + this.url + '?imageView2/1/w/100/h/100" width="100px" height="100px" class="img-rounded" data-id="' + this.id + '">' +
         '</li>';
       $(".selected-container").append(imageElement);
     }else{
+      //delete the cropped image
       $picShadow.hide();
       var selected = $(".selected-picture-container");
+      var flag;
       for (var i = 0;i < selected.length;i++){
-        if ($(selected[i]).children("img").attr("data-index") == this.index){
+        if ($(selected[i]).attr("data-index") == this.index){
           flag = true;
           $(selected[i]).remove();
           break;
         }
       }
     }
-    
   },
 
   "click .raw-picture-container": function(e){
@@ -67,7 +71,106 @@ Template.pictures.events({
 
   "click .btn-pic-sort": function(e){
     $('.selected-container').sortable().bind('sortupdate');
-  }
+  },
+
+  "click .btn-pic-submit": function(e){
+    var selected = $(".selected-picture-container");
+    console.log(cropHints);
+    for (var i = 0;i < selected.length;i++){
+      console.log($(selected[i]).attr("data-index"));
+      console.log(cropHints[ $(selected[i]).attr("data-index") ]);
+    }
+  },
+
+  "click #pic-up-sub": function(e){
+    Meteor.call('getPicUpToken', function(error, result) {
+      if (error) {
+        return throwError(error.reason);
+      }
+      if (result){
+        $("#picUpToken").val(result.upToken);
+        $("#picUpKey").val(result.key);
+        var form_data = new FormData($('#pic-up')[0]);
+        $.ajax({
+          type: 'post',
+          url: 'http://upload.qiniu.com/',
+          async: false,
+          cache: false,
+          contentType: false,
+          processData: false,
+          data: form_data,
+          success: function(data) {
+            var image_url = "http://7xi9ns.com1.z0.glb.clouddn.com/";
+            console.log(data);
+            alert("上传成功！链接：" + image_url + data.key);
+          }
+        });
+      }else{
+        console.log("获取token失败！");
+        alert("上传图片失败，请再次上传或联系程序员！");
+      }
+    });
+  },
+
+  "click #pic-fet-sub": function(e){
+    var fetchUrl = $('#fet-pic-url').val();
+    Meteor.call('fetchPic', fetchUrl, function(error, result) {
+      if (error) {
+        return throwError(error.reason);
+      }
+      if (result){
+        alert("成功上传图片！");
+      }else{
+        alert("上传图片失败，请再次上传或联系程序员！");   
+      }
+    });
+
+    // if (fetch){
+      // alert("保存成功,链接为:" + fetchUrl);
+    // }
+
+    // var encodedURL = "aHR0cHM6Ly9zczEuYmFpZHUuY29tLzl2bzNkU2FnX3hJNGtoR2tvOVdUQW5GNmhoeS9zdXBlci93aGZwZiUzRDQyNSUyQzI2MCUyQzUwL3NpZ249ZGNkOGUxODA3N2YwODIwMjJkYzdjMjdmMmRjNmNmZGYvZGM1NDU2NGU5MjU4ZDEwOWMyNGRhYzExZDU1OGNjYmY2ZDgxNGRlMi5qcGc=";
+    // var encodedEntryURI = "aG9wZWxlZnQ6YWJjZGU=";
+    // var path = '/fetch/' + encodedURL + '/to/' + encodedEntryURI;
+    // var accessToken = 'TchpexGkbyuY0nMt-T1_xIpbsgN90lBg3QyD3utE:7-GNext63FOeELw-ahdI8I-4p34=';
+
+    // var url = 'http://iovip.qbox.me' + path;
+    // var options = {
+    //   headers: {
+    //     'Authorization': 'QBox TchpexGkbyuY0nMt-T1_xIpbsgN90lBg3QyD3utE:7-GNext63FOeELw-ahdI8I-4p34=',
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //   }
+    // };
+
+    // $.ajax({
+    //   type: 'post',
+    //   url: 'http://iovip.qbox.me' + path,
+    //   beforeSend:function(xhr){
+    //     xhr.setRequestHeader('Authorization', "QBox " + accessToken);
+    //     console.log(xhr);
+    //   },
+    //   // POST /fetch/<EncodedURL>/to/<EncodedEntryURI>
+    //   // async: false,
+    //   // cache: false,
+    //   // contentType: false,
+    //   contentType: 'application/x-www-form-urlencoded',
+    //   // processData: false,
+    //   // data: form_data,
+    //   // data: null,
+    //   // Authorization: QBox <AccessToken>,
+    //   // headers: {
+    //   //   'Authorization': "QBox " + AccessToken
+    //   // },
+    //   // beforeSend:function(xhr){
+    //   //   xhr.setRequestHeader('Authorization', "QBox " + AccessToken);
+    //   //   console.log(xhr);
+    //   // },
+    //   success: function(data) {
+    //     var image_url = "http://7xi9ns.com1.z0.glb.clouddn.com/";
+    //     console.log(image_url + "abcde");
+    //   }
+    // });
+  },
 })
 
 //show cropWindow
--- server/publications/pictures.js
+++ server/publications/pictures.js
@@ -10,3 +10,83 @@ Meteor.publish('Images', function(geoId) {
   // db.Images.find({itemIds:ObjectId("5492bd35e721e7171745165b")})
 });
 
+//config.file
+var expires = 1800;//30mins
+var returnBody = '{' +
+  '"name": $(fname),' +
+  '"size": $(fsize),' +
+  '"w": $(imageInfo.width),' +
+  '"h": $(imageInfo.height),' +
+  '"hash": $(etag)' +
+'}';
+
+
+var crypto = Npm.require('crypto');
+Meteor.methods({
+  'fetchPic': function(fetchUrl){
+    check(fetchUrl, String);
+    var encodedURL = base64ToUrlSafe(new Buffer(fetchUrl).toString('base64'));//from url
+    var key = crypto.createHash('md5').update(fetchUrl).digest('hex');
+    var entry = bucket + ':' + key;
+    var encodedEntryURI = base64ToUrlSafe(new Buffer(entry).toString('base64'));//to space
+    var path = '/fetch/' + encodedURL + '/to/' + encodedEntryURI;
+    var signingStr = path + '\n';
+    //hamac_sha1
+    var sign = crypto.createHmac("sha1", secretKey).update(signingStr).digest('base64');
+    //base64encdode
+    var encodedSign = base64ToUrlSafe(sign);
+    var accessToken = accessKey + ":" + encodedSign;
+
+    //post
+    var postUrl = 'http://iovip.qbox.me' + path;
+    var options = {
+      headers: {
+        'Authorization': 'QBox ' + accessToken,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    };
+
+    try{
+      var result = HTTP.call('POST', postUrl, options);
+      return result.statusCode === 200;
+    }catch(e){
+      return false;
+    }
+  },
+  'getPicUpToken': function(){
+    var expire = expires || 3600;
+    var deadline = expire + Math.floor(Date.now() / 1000);
+    var putPolicy = {
+          scope: bucket,
+          deadline: deadline,
+          returnBody: returnBody
+        };
+    var flags = JSON.stringify(putPolicy);
+    var encodedFlags = base64ToUrlSafe(new Buffer(flags).toString('base64'));
+    var encoded = crypto.createHmac("sha1", secretKey).update(encodedFlags).digest('base64');
+    var encodedSign = base64ToUrlSafe(encoded);
+    // var encodedSign = base64ToUrlSafe(new Buffer(encoded).toString('base64'));说好的加密呢？
+    var upToken = accessKey + ':' + encodedSign + ':' + encodedFlags;
+
+    var id = uuid.v1();
+    // var id2 = uuid.v4();s
+    return {
+      upToken: upToken,
+      key: id
+    };
+  }
+})
+
+function base64ToUrlSafe(v){
+  return v.replace(/\//g, '_').replace(/\+/g, '-');
+}
+
+
+
+
+
+
+
+
+
+
