c89faabe6cbcd7ba6cccc29c0288e46a978e44aa
Zephyre <haizi.zh@gmail.com>
2015-04-02 20:45:48 +0800

    MOD: minor

--- server/publications/storage_engine.js
+++ server/publications/storage_engine.js
@@ -1,14 +1,14 @@
 // var needed?
-OplogCol = new Mongo.Collection('Oplog');
+var OplogCol = new Mongo.Collection('Oplog');
 
 /**
  * 发布oplog信息
- * @param  {[string]} ns     [oplog对应的namespace，如'geo.Locality']
- * @param  {[ObjectId]} pk     [oplog对应数据的主键]
- * @param {[int]} [count] [取最后多少条记录。如果为undefined，则取所有记录]
+ * @param  {string} ns     oplog对应的namespace，如'geo.Locality'
+ * @param  {ObjectId} pk     oplog对应数据的主键
+ * @param  {int} count 取最后多少条记录。如果为undefined，则取所有记录
  */
 Meteor.publish('oplog', function(ns, pk, count) {
-  var query = {'ns': ns, 'pk': pk}
+  var query = {'ns': ns, 'pk': pk};
   if (range === undefined) {
     return OplogCol.find(query, {sort: {ts: -1}});
   } else {
