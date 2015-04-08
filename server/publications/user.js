17f697d607be81e6eddac038a6a8215b707f8db8
ck <chenkaiC4@gmail.com>
2015-04-08 18:44:51 +0800

    filter in recheck

--- server/publications/user.js
+++ server/publications/user.js
@@ -12,25 +12,46 @@ Meteor.publish("userData", function() {
   }
 });
 
-Meteor.publish('allUsers', function(){
-  return Meteor.users.find({}, {fields: {'rights': 1, 'username': 1}});
+Meteor.publish('allUsers', function() {
+  return Meteor.users.find({}, {
+    fields: {
+      'rights': 1,
+      'username': 1
+    }
+  });
+});
+
+Meteor.publish('editor', function() {
+  return Meteor.users.find({'rights': "editor"}, {fields: {'username': 1, '_id': 1, 'rights': 1}});
 });
 
 Meteor.methods({
   addUserRight: function(rightName, uid) {
     check(rightName, String);
     check(uid, String);
-    Meteor.users.update({'_id': uid}, {'$addToSet': {'rights': rightName}});
+    Meteor.users.update({
+      '_id': uid
+    }, {
+      '$addToSet': {
+        'rights': rightName
+      }
+    });
   },
   removeUserRight: function(rightName, uid) {
     check(rightName, String);
     check(uid, String);
-    Meteor.users.update({'_id': uid}, {'$pullAll': {'rights': [rightName,]}});
+    Meteor.users.update({
+      '_id': uid
+    }, {
+      '$pullAll': {
+        'rights': [rightName, ]
+      }
+    });
   },
 });
 
 Meteor.users.allow({
-  update: function(){
+  update: function() {
     return true;
   }
 })
