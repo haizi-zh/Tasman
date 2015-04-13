9f71405e459a4ef2800d3fb3e7733aeb3c83b577
lyn <buaaxigua@gmail.com>
2015-04-13 20:51:48 +0800

    add the hotel and add the auto-size

--- client/templates/data_review/includes/tpl/itemTpl.js
+++ client/templates/data_review/includes/tpl/itemTpl.js
@@ -1,5 +1,6 @@
 Template.stringTpl.helpers({
   UpdateMD5: function() {
+    // $('textarea').flexText();
     return function(e, editor) {
       var curHTML = editor.getHTML(); // 嵌入了编译器的标签
       var curText = editor.getText(); // 去除了所有html标签（包括本身就带有的），只有纯文本，
@@ -12,6 +13,18 @@ Template.stringTpl.helpers({
 });
 
 Template.stringTpl.onRendered(function() {
+  // 有奇怪的问题:行数太多时会有点不够长的现象，大部分情况都还好
+  $('textarea').flexText();
+  // 第一次可以成功(onrendered)
+  // autosize(document.querySelectorAll('textarea'));
+  // 没成功...
+  // jQuery.each(jQuery('textarea[data-autoresize]'), function() {
+  //   var offset = this.offsetHeight - this.clientHeight;
+  //   var resizeTextarea = function(el) {
+  //       jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+  //   };
+  //   jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+  // });
   if (!this.data.richEditor) {
     var keyChain = this.data.keyChain;
     $('textarea#' + keyChain).on('blur', function(e) {
@@ -20,6 +33,14 @@ Template.stringTpl.onRendered(function() {
     })
   }
 });
+
+/*
+  为了auto-size of textarea
+ */
+// Template.stringTpl.rendered = function(){
+//   autosize(document.querySelectorAll('textarea'));
+//   autosize($('textarea'));
+// }
 
 /*
 @ key: 数据库中的字段名
