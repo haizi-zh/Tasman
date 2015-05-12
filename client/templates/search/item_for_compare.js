faff377f05311a327984fcb990789928f81cd8ea
ck <chenkaiC4@gmail.com>
2015-05-12 21:48:21 +0800

    improve poi merge logic and UI

--- client/templates/search/item_for_compare.js
+++ client/templates/search/item_for_compare.js
@@ -1,7 +1,9 @@
 Template.compareItem.events({
   'click .selected_item': function(e) {
+    e.preventDefault();
+    e.stopPropagation();
     var curDOm = $(e.target),
-        id = curDOm.parent().attr('id');
+        id = curDOm.attr('id');
     Session.set('compareItems', _.without(Session.get('compareItems'), id));
     curDOm.parent().remove();
     $('input[type=checkbox]').each(function(index){
